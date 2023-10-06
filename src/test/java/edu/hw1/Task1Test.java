31ce514a24fe32e4565808591350b182dca2e4e5
AntonioRusan <antonrusan@gmail.com>
2023-10-06 22:59:44 +0300

    Task1Test complete

--- src/test/java/edu/hw1/Task1Test.java
+++ src/test/java/edu/hw1/Task1Test.java
@@ -1,85 +1,26 @@
 package edu.hw1;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task1Test {
-    @Test
-    @DisplayName("Время с 1 минутой 0 секунд")
-    void zeroSecondsTime() {
-        // given
-        String str = "01:00";
-
+    @ParameterizedTest
+    @CsvSource({
+        "01:00, 60",
+        "00:00, 0",
+        "13:56, 836",
+        "10:60, -1",
+        "999:59, 59999",
+        "55:g6, -1",
+        "100.12, -1",
+        ":, -1"
+    })
+    void minutesToSecondsCsvSourceTest(String timeLength, int expectedTotalTime) {
         // when
-        int result = Task1.minutesToSeconds(str);
+        int result = Task1.minutesToSeconds(timeLength);
 
         // then
-        assertThat(result).isEqualTo(60);
-    }
-
-    @Test
-    @DisplayName("Время 0 минут 0 секунд")
-    void zeroTime() {
-        // given
-        String str = "00:00";
-
-        // when
-        int result = Task1.minutesToSeconds(str);
-
-        // then
-        assertThat(result).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("Обычное время")
-    void normalTime() {
-        // given
-        String str = "13:56";
-
-        // when
-        int result = Task1.minutesToSeconds(str);
-
-        // then
-        assertThat(result).isEqualTo(836);
-    }
-
-    @Test
-    @DisplayName("Число секунд >= 60")
-    void tooMuchSeconds() {
-        // given
-        String str = "10:60";
-
-        // when
-        int result = Task1.minutesToSeconds(str);
-
-        // then
-        assertThat(result).isEqualTo(-1);
-    }
-
-    @Test
-    @DisplayName("Минуты никак не ограничены")
-    void manyMinutes() {
-        // given
-        String str = "999:59";
-
-        // when
-        int result = Task1.minutesToSeconds(str);
-
-        // then
-        assertThat(result).isEqualTo(59999);
-    }
-
-    @Test
-    @DisplayName("Строка не формата mm:ss")
-    void notTimeString() {
-        // given
-        String str = "55:g6";
-
-        // when
-        int result = Task1.minutesToSeconds(str);
-
-        // then
-        assertThat(result).isEqualTo(-1);
+        assertThat(result).isEqualTo(expectedTotalTime);
     }
 }
