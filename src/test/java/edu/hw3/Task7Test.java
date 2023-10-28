480512aa564b0553aec67cb00032dd90c889d9fd
AntonioRusan <antonrusan@gmail.com>
2023-10-28 15:04:30 +0300

    Task7 newTest

--- src/test/java/edu/hw3/Task7Test.java
+++ src/test/java/edu/hw3/Task7Test.java
@@ -1,5 +1,6 @@
 package edu.hw3;
 
+import java.util.Set;
 import java.util.TreeMap;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class Task7Test {
     @Test
     @DisplayName("Тест добавления null ключа")
-    void removeStockTest() {
+    void nullInTreeMapTest() {
         // given
         TreeMap<String, String> tree = new TreeMap<>(new Task7.NullComparator<>());
         // when
@@ -16,5 +17,21 @@ public class Task7Test {
         // then
         assertThat(tree.containsKey(null)).isTrue();
         assertThat(tree.get(null)).isEqualTo("test");
+    }
+
+    @Test
+    @DisplayName("Проверка сортировки в TreeMap для null")
+    void orderInTreeMapTest() {
+        // given
+        TreeMap<String, String> tree = new TreeMap<>(new Task7.NullComparator<>());
+        String[] expected = new String[] {null, "first", "second"};
+        // when
+        tree.put("second", "testSecond");
+        tree.put(null, "test");
+        tree.put("first", "testFirst");
+        Set<String> keys = tree.keySet();
+        // then
+        assertThat(tree.containsKey(null)).isTrue();
+        assertThat(keys.toArray()).isEqualTo(expected);
     }
 }
