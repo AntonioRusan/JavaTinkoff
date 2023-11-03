09afc11cb10576efce9a47525dbeca525fdb8e7a
AntonioRusan <antonrusan@gmail.com>
2023-11-03 20:52:04 +0300

    Task3 fixed

--- src/main/java/edu/hw3/Task3.java
+++ src/main/java/edu/hw3/Task3.java
@@ -10,12 +10,8 @@ public class Task3 {
     public static <T> HashMap<T, Integer> frequencyDict(ArrayList<T> inputList) {
         HashMap<T, Integer> resultMap = new HashMap<>();
         for (var item : inputList) {
-            if (resultMap.containsKey(item)) {
-                var newItem = resultMap.get(item) + 1;
-                resultMap.put(item, newItem);
-            } else {
-                resultMap.put(item, 1);
-            }
+            var newItem = resultMap.getOrDefault(item, 0) + 1;
+            resultMap.put(item, newItem);
         }
         return resultMap;
     }
