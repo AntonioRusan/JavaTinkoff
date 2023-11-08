1fad17b7915e3f6b83d73acbff945e185f1a59bb
AntonioRusan <antonrusan@gmail.com>
2023-11-08 22:22:43 +0300

    Task8 complete

--- src/main/java/edu/hw5/Task8.java
+++ src/main/java/edu/hw5/Task8.java
@@ -21,13 +21,9 @@ public class Task8 {
     }
 
     public static boolean numberOfZeroDivisible3(String input) {
-        Pattern pattern = Pattern.compile("0");
+        Pattern pattern = Pattern.compile("^1*(1*01*01*01*)*");
         Matcher matcher = pattern.matcher(input);
-        int countOfZero = 0;
-        while (matcher.find()) {
-            countOfZero++;
-        }
-        return countOfZero % 3 == 0;
+        return matcher.matches();
     }
 
     public static boolean notTwoOrThreeOnes(String input) {
@@ -35,8 +31,23 @@ public class Task8 {
         Matcher matcher = pattern.matcher(input);
         return matcher.matches();
     }
+
     public static boolean everyOddIsOne(String input) {
         Pattern pattern = Pattern.compile("^((1[01])*|1([01]1)*)$");
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.matches();
+    }
+
+    public static boolean noSubsequentOnes(String input) {
+        Pattern pattern = Pattern.compile("^(0*(10+)*1?|1(0+1)*0*)$");
+        Matcher matcher = pattern.matcher(input);
+
+        return matcher.matches();
+    }
+
+    public static boolean moreThan1ZerosLessThan2One(String input) {
+        Pattern pattern = Pattern.compile("^(10{2,}|0*01?0+|0{2,}1?)$");
         Matcher matcher = pattern.matcher(input);
 
         return matcher.matches();
