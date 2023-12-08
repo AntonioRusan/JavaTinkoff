05fd27684279dcdfce5e9013f2f5e51f3a0008a3
AntonioRusan <antonrusan@gmail.com>
2023-12-08 11:03:38 +0300

    first commit

---
+++ src/main/java/edu/project4/Pixel.java
@@ -0,0 +1,8 @@
+package edu.project4;
+
+import java.util.function.Function;
+
+public record Pixel(int r, int g, int b, int hitCount) { }
+
+
+
---
+++ src/main/java/edu/project4/Transformation.java
@@ -0,0 +1,7 @@
+package edu.project4;
+
+import java.util.function.Function;
+
+// функция-преобразование
+public interface Transformation extends Function<Point, Point> {
+}
