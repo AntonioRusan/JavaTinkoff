d6e46b02ddc15d1c58efc213b591712ca3b69a4b
AntonioRusan <antonrusan@gmail.com>
2023-12-16 02:03:53 +0300

    task1 fully completed

--- src/main/java/edu/hw10/RandomObjectGenerator.java
+++ src/main/java/edu/hw10/RandomObjectGenerator.java
@@ -4,66 +4,22 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
-
 
 public class RandomObjectGenerator {
     private static final int STRING_LENGTH = 20;
     private static final int NULL_PROBABILITY = 4;
 
-    public <T> T nextObject(Class<T> inputClass, String factoryMethodName) {
-        try {
-            Method factoryMethod = inputClass.getMethod(factoryMethodName);
-            Field[] fields = inputClass.getFields();
-            T instance = (T) factoryMethod.invoke(null);
-            for (Field field : fields) {
-                boolean notNull = false;
-                int minValue = Integer.MIN_VALUE;
-                int maxValue = Integer.MAX_VALUE;
-                notNull = field.isAnnotationPresent(NotNull.class);
-                if (field.isAnnotationPresent(Min.class)) {
-                    minValue = field.getAnnotation(Min.class).value();
-                }
-                if (field.isAnnotationPresent(Max.class)) {
-                    maxValue = field.getAnnotation(Max.class).value();
-                }
-                setField(field, instance, minValue, maxValue, notNull);
-            }
-            return instance;
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public <T> T nextObject(Class<T> inputClass) {
-        try {
-            Constructor<T> constructor = inputClass.getDeclaredConstructor();
-            Field[] fields = inputClass.getDeclaredFields();
-            T instance = constructor.newInstance();
-
-            for (Field field : fields) {
-                boolean notNull = false;
-                int minValue = Integer.MIN_VALUE;
-                int maxValue = Integer.MAX_VALUE;
-                notNull = field.isAnnotationPresent(NotNull.class);
-                if (field.isAnnotationPresent(Min.class)) {
-                    minValue = field.getAnnotation(Min.class).value();
-                }
-                if (field.isAnnotationPresent(Max.class)) {
-                    maxValue = field.getAnnotation(Max.class).value();
-                }
-                setField(field, instance, minValue, maxValue, notNull);
-            }
-            return instance;
-        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException
-                 | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private static final int MIN_VALUE = Integer.MIN_VALUE / 3;
+    private static final int MAX_VALUE = Integer.MAX_VALUE / 3;
 
     public <T> void setField(Field field, T instance, int min, int max, boolean notNull) {
         Random random = new Random();
@@ -111,6 +67,123 @@ public class RandomObjectGenerator {
             }
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T nextObject(Class<T> inputClass, String factoryMethodName) {
+        try {
+            List<Method> methods = Arrays.asList(inputClass.getDeclaredMethods());
+            Method factoryMethod =
+                methods.stream().filter(method -> method.getName().equals(factoryMethodName)).findFirst().orElse(null);
+            Field[] fields = inputClass.getFields();
+            List<T> fieldValueList = new ArrayList<>();
+            for (Field field : fields) {
+                boolean notNull = false;
+                int minValue = Integer.MIN_VALUE;
+                int maxValue = Integer.MAX_VALUE;
+                notNull = field.isAnnotationPresent(NotNull.class);
+                if (field.isAnnotationPresent(Min.class)) {
+                    minValue = field.getAnnotation(Min.class).value();
+                }
+                if (field.isAnnotationPresent(Max.class)) {
+                    maxValue = field.getAnnotation(Max.class).value();
+                }
+                fieldValueList.add((T) getFieldValue(field.getType(), minValue, maxValue, notNull));
+            }
+            T instance = (T) factoryMethod.invoke(null, fieldValueList.toArray());
+            return instance;
+        } catch (InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T nextObject(Class<T> inputClass) {
+        try {
+            Field[] fields = inputClass.getDeclaredFields();
+
+            Map<Class<T>, T> fieldToValue = new LinkedHashMap<>();
+            for (Field field : fields) {
+                boolean notNull = false;
+                int minValue = MIN_VALUE;
+                int maxValue = MAX_VALUE;
+                notNull = field.isAnnotationPresent(NotNull.class);
+                if (field.isAnnotationPresent(Min.class)) {
+                    minValue = field.getAnnotation(Min.class).value();
+                }
+                if (field.isAnnotationPresent(Max.class)) {
+                    maxValue = field.getAnnotation(Max.class).value();
+                }
+                fieldToValue.put(
+                    (Class<T>) field.getType(),
+                    (T) getFieldValue(field.getType(), minValue, maxValue, notNull)
+                );
+            }
+            Class<?>[] fieldList = new Class<?>[fieldToValue.entrySet().size()];
+            int ind = 0;
+            for (var pair : fieldToValue.entrySet()) {
+                fieldList[ind] = pair.getKey();
+                ind++;
+            }
+            var constr = inputClass.getDeclaredConstructor(fieldList);
+            T instance = constr.newInstance(fieldToValue.values().toArray());
+            return instance;
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException
+                 | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T getFieldValue(Class<T> fieldType, int min, int max, boolean notNull) {
+        Random random = new Random();
+        if (notNull) {
+            return getFieldValueByType(fieldType, min, max);
+        } else {
+            int maxNull = NULL_PROBABILITY;
+            int minNull = 0;
+            int randomNum = random.nextInt((maxNull - minNull) + 1) + minNull;
+            if (randomNum == 0) {
+                return null;
+            } else {
+                return getFieldValueByType(fieldType, min, max);
+            }
+        }
+
+    }
+
+    @SuppressWarnings("ReturnCount")
+    private <T> T getFieldValueByType(Class<T> fieldType, int min, int max) {
+        Random random = new Random();
+        byte[] array = new byte[STRING_LENGTH];
+        String fieldName = fieldType.getSimpleName();
+        ClassNames classNameEnum = ClassNames.valueOf(fieldName.toUpperCase());
+        switch (classNameEnum) {
+            case INTEGER -> {
+                Integer intValue = random.nextInt((max - min) + 1) + min;
+                return (T) intValue;
+            }
+            case LONG -> {
+                Long longValue = random.nextLong((max - min) + 1) + min;
+                return (T) longValue;
+            }
+            case DOUBLE -> {
+                Double doubleValue = random.nextDouble((max - min) + 1) + min;
+                return (T) doubleValue;
+            }
+            case CHARACTER -> {
+                array = new byte[1];
+                new Random().nextBytes(array);
+                Character charValue = new String(array).charAt(0);
+                return (T) charValue;
+            }
+            case STRING -> {
+                new Random().nextBytes(array);
+                return (T) new String(array);
+            }
+            case BOOLEAN -> {
+                Boolean boolValue = random.nextBoolean();
+                return (T) boolValue;
+            }
+            default -> throw new RuntimeException();
         }
     }
 
--- src/test/java/edu/hw10/RandomObjectGeneratorTest.java
+++ src/test/java/edu/hw10/RandomObjectGeneratorTest.java
@@ -11,24 +11,34 @@ public class RandomObjectGeneratorTest {
         @RandomObjectGenerator.NotNull
         @RandomObjectGenerator.Min(0)
         @RandomObjectGenerator.Max(150)
-        public int age;
+        public Integer age;
 
         @RandomObjectGenerator.Min(0)
         @RandomObjectGenerator.Max(200)
-        public Integer height;
+        public Double height;
 
         public Person() {
         }
 
-        public Person(String name, int age, Integer height) {
+        public Person(String name, Integer age, Double height) {
             this.name = name;
             this.age = age;
             this.height = height;
         }
 
-        public static Person create() {
-            return new Person();
+        public static Person create(String name, Integer age, Double height) {
+            return new Person(name, age, height);
         }
+    }
+
+    public record PersonRecord(
+        @RandomObjectGenerator.NotNull String name,
+        @RandomObjectGenerator.NotNull
+        @RandomObjectGenerator.Min(0)
+        @RandomObjectGenerator.Max(150) Integer age,
+        @RandomObjectGenerator.Min(0)
+        @RandomObjectGenerator.Max(200)
+        Double height) {
     }
 
     @Test
@@ -49,5 +59,15 @@ public class RandomObjectGeneratorTest {
         assertNotNull(person.name);
         assertTrue(person.age >= 0 && person.age <= 150);
         assertTrue(person.height == null || (person.height >= 0 && person.height <= 200));
+    }
+
+    @Test
+    void testNextObjectRecord() {
+        RandomObjectGenerator rog = new RandomObjectGenerator();
+        PersonRecord personRecord = rog.nextObject(PersonRecord.class);
+        assertNotNull(personRecord);
+        assertNotNull(personRecord.name);
+        assertTrue(personRecord.age >= 0 && personRecord.age <= 150);
+        assertTrue(personRecord.height == null || (personRecord.height >= 0 && personRecord.height <= 200));
     }
 }
