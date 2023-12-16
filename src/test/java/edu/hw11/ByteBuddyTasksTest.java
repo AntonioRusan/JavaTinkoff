ade05553241bd62eb7b3a2709b04d38918b5fd65
AntonioRusan <antonrusan@gmail.com>
2023-12-16 23:09:47 +0300

    jacoco fixing4

--- src/test/java/edu/hw11/ByteBuddyTasksTest.java
+++ src/test/java/edu/hw11/ByteBuddyTasksTest.java
@@ -2,31 +2,14 @@ package edu.hw11;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.ByteBuddyAgent;
-import net.bytebuddy.description.method.MethodDescription;
-import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
 import net.bytebuddy.implementation.FixedValue;
-import net.bytebuddy.implementation.Implementation;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.implementation.bytecode.ByteCodeAppender;
-import net.bytebuddy.jar.asm.Label;
-import net.bytebuddy.jar.asm.MethodVisitor;
-import net.bytebuddy.jar.asm.Opcodes;
-import net.bytebuddy.matcher.ElementMatchers;
 import org.junit.jupiter.api.Test;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.lang.reflect.Method;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ByteBuddyTasksTest.Generated
+@Generated
 public class ByteBuddyTasksTest {
     @Test
     public void task1HelloTest() throws Exception {
@@ -40,13 +23,11 @@ public class ByteBuddyTasksTest {
         assertThat(dynamicType.getDeclaredConstructor().newInstance().toString()).isEqualTo("Hello, ByteBuddy!");
     }
 
-
     public static class ArithmeticUtils {
         public int sum(int a, int b) {
             return a + b;
         }
     }
-
 
     public static class MultiplicationInterceptor {
         public int sum(int a, int b) {
@@ -70,10 +51,4 @@ public class ByteBuddyTasksTest {
         assertThat(utils.sum(2, 3)).isEqualTo(6);
     }
 
-    //Аннотация, чтобы исключить генерируемые классы из jaccoco и не было ошибок на github
-    @Documented
-    @Retention(RUNTIME)
-    @Target({TYPE, METHOD, CONSTRUCTOR})
-    public @interface Generated {
-    }
 }
---
+++ src/test/java/edu/hw11/Generated.java
@@ -0,0 +1,16 @@
+package edu.hw11;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+//Аннотация, чтобы исключить генерируемые классы из jaccoco и не было ошибок на github
+@Documented
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, CONSTRUCTOR})
+public @interface Generated {
+}
