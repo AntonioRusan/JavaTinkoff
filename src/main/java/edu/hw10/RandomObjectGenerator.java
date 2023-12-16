ad2b1761d8b7ee15ba1a3f4493c9fc37e4d00aab
AntonioRusan <antonrusan@gmail.com>
2023-12-16 16:04:40 +0300

    task1 fix

--- src/main/java/edu/hw10/RandomObjectGenerator.java
+++ src/main/java/edu/hw10/RandomObjectGenerator.java
@@ -21,55 +21,6 @@ public class RandomObjectGenerator {
     private static final int MIN_VALUE = Integer.MIN_VALUE / 3;
     private static final int MAX_VALUE = Integer.MAX_VALUE / 3;
 
-    public <T> void setField(Field field, T instance, int min, int max, boolean notNull) {
-        Random random = new Random();
-        try {
-            if (notNull) {
-                setFieldByType(field, instance, min, max);
-            } else {
-                int maxNull = NULL_PROBABILITY;
-                int minNull = 0;
-                int randomNum = random.nextInt((maxNull - minNull) + 1) + minNull;
-                if (randomNum == 0) {
-                    field.set(instance, null);
-                } else {
-                    setFieldByType(field, instance, min, max);
-                }
-            }
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private <T> void setFieldByType(Field field, T instance, int min, int max) {
-        try {
-            Random random = new Random();
-            byte[] array = new byte[STRING_LENGTH];
-            String fieldName = field.getType().getSimpleName();
-            ClassNames classNameEnum = ClassNames.valueOf(fieldName.toUpperCase());
-            switch (classNameEnum) {
-                case INT -> field.setInt(instance, random.nextInt((max - min) + 1) + min);
-                case INTEGER -> field.set(instance, random.nextInt((max - min) + 1) + min);
-                case LONG -> field.setLong(instance, random.nextLong((max - min) + 1) + min);
-                case DOUBLE -> field.setDouble(instance, random.nextDouble((max - min) + 1) + min);
-                case CHARACTER -> {
-                    array = new byte[1];
-                    new Random().nextBytes(array);
-                    field.setChar(instance, new String(array).charAt(0));
-                }
-                case STRING -> {
-                    new Random().nextBytes(array);
-                    field.set(instance, new String(array));
-                }
-                case BOOLEAN -> field.setBoolean(instance, random.nextBoolean());
-                default -> throw new RuntimeException();
-            }
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public <T> T nextObject(Class<T> inputClass, String factoryMethodName) {
         try {
             List<Method> methods = Arrays.asList(inputClass.getDeclaredMethods());
