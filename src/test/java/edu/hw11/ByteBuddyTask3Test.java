e149415b3ef96db37cc223b540f7bbfced8965e8
AntonioRusan <antonrusan@gmail.com>
2023-12-17 22:17:51 +0300

    some fixes

--- src/test/java/edu/hw11/ByteBuddyTask3Test.java
+++ src/test/java/edu/hw11/ByteBuddyTask3Test.java
@@ -1,24 +1,19 @@
 package edu.hw11;
 
+import java.lang.reflect.Method;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.method.MethodDescription;
-import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.Implementation;
-import net.bytebuddy.implementation.MethodDelegation;
 import net.bytebuddy.implementation.bytecode.ByteCodeAppender;
 import net.bytebuddy.jar.asm.Label;
 import net.bytebuddy.jar.asm.MethodVisitor;
 import net.bytebuddy.jar.asm.Opcodes;
-import net.bytebuddy.matcher.ElementMatchers;
 import org.junit.jupiter.api.Test;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class ByteBuddyTask3Test {
     @Test
     public void task3() throws Exception {
-        /*Class<?> loadedClass = new ByteBuddy()
+        Class<?> loadedClass = new ByteBuddy()
             .subclass(Object.class)
             .name("Fibonacci")
             .defineMethod("fib", int.class, Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC)
@@ -28,52 +23,60 @@ public class ByteBuddyTask3Test {
             .load(ByteBuddyTask3Test.class.getClassLoader())
             .getLoaded();
 
-        Method[] methods = loadedClass.getDeclaredMethods();
-        Method fibMethod = loadedClass.getMethod("fib", int.class);
+        Method fibMethod = loadedClass.getDeclaredMethod("fib", int.class);
         int result = (int) fibMethod.invoke(null, 10);
-        System.out.println(result);*/
+        System.out.println(result);
     }
 
     public static class FibonacciByteCodeAppender implements ByteCodeAppender {
         @Override
-        public Size apply(MethodVisitor methodVisitor, Implementation.Context context, MethodDescription instrumentedMethod) {
+        public Size apply(
+            MethodVisitor methodVisitor,
+            Implementation.Context context,
+            MethodDescription instrumentedMethod
+        ) {
             methodVisitor.visitCode();
-            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
-            //methodVisitor.visitJumpInsn(Opcodes.IFLE, new Label());
 
+            Label start = new Label();
+            Label loopCheck = new Label();
+            Label loopBody = new Label();
+            Label end = new Label();
+
+            methodVisitor.visitLabel(start);
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
+            methodVisitor.visitJumpInsn(Opcodes.IFLE, end);
             methodVisitor.visitInsn(Opcodes.ICONST_1);
             methodVisitor.visitVarInsn(Opcodes.ISTORE, 2);
             methodVisitor.visitInsn(Opcodes.ICONST_1);
             methodVisitor.visitVarInsn(Opcodes.ISTORE, 3);
 
-            Label loopStart = new Label();
-            Label loopEnd = new Label();
-
-            methodVisitor.visitLabel(loopStart);
+            methodVisitor.visitLabel(loopCheck);
             methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
-            methodVisitor.visitInsn(Opcodes.ICONST_1);
-            methodVisitor.visitVarInsn(Opcodes.ISTORE, 1);
+            methodVisitor.visitJumpInsn(Opcodes.IFLE, end);
 
             methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
             methodVisitor.visitVarInsn(Opcodes.ILOAD, 3);
             methodVisitor.visitInsn(Opcodes.IADD);
-            methodVisitor.visitVarInsn(Opcodes.ISTORE, 2);
+            methodVisitor.visitVarInsn(Opcodes.ISTORE, 4);
 
             methodVisitor.visitVarInsn(Opcodes.ILOAD, 3);
-            methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
+            methodVisitor.visitVarInsn(Opcodes.ISTORE, 2);
+
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 4);
             methodVisitor.visitVarInsn(Opcodes.ISTORE, 3);
 
-            methodVisitor.visitVarInsn(Opcodes.ILOAD, 1);
-            methodVisitor.visitJumpInsn(Opcodes.IFLE, loopEnd);
-            methodVisitor.visitJumpInsn(Opcodes.GOTO, loopStart);
+            methodVisitor.visitJumpInsn(Opcodes.GOTO, loopBody);
 
-            methodVisitor.visitLabel(loopEnd);
-            methodVisitor.visitVarInsn(Opcodes.ILOAD, 2);
+            methodVisitor.visitLabel(loopBody);
+            methodVisitor.visitJumpInsn(Opcodes.GOTO, loopCheck);
+
+            methodVisitor.visitLabel(end);
+            methodVisitor.visitVarInsn(Opcodes.ILOAD, 3);
             methodVisitor.visitInsn(Opcodes.IRETURN);
 
-            methodVisitor.visitMaxs(2, 4);
+            methodVisitor.visitMaxs(2, 5);
             methodVisitor.visitEnd();
-            return new Size(2, 4);
+            return new Size(2, 5);
         }
     }
 }
